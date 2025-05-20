d64f13d74c74745a336a2e593532570cb1aae30a
rayan <desmetrayan621@gmail.com>
2025-05-20 18:10:58 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: '/NOM_DU_REPO/', // ex: '/NFT/'
+   base: '/eShop/'
 })
